e01e5d4038ac53e33bbaecb3501a4785d8fd17b4
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2023-12-06 15:40:30 -0300

    Learn to Future-proof DApps with ZetaChain - Write A Cross-Chain Swap Omnichain Contract

---
+++ zetachain_contracts/template-swap/contracts/Swap.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
+import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
+import "@zetachain/toolkit/contracts/SwapHelperLib.sol";
+import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
+
+contract Swap is zContract {
+    SystemContract public immutable systemContract;
+    uint256 constant BITCOIN = 18332;
+    error WrongGasContract();
+    error NotEnoughToPayGasFee();
+
+    constructor(address systemContractAddress) {
+        systemContract = SystemContract(systemContractAddress);
+    }
+
+    modifier onlySystem() {
+        require(
+            msg.sender == address(systemContract),
+            "Only system contract can call this function"
+        );
+        _;
+    }
+
+    function onCrossChainCall(
+        zContext calldata context,
+        address zrc20,
+        uint256 amount,
+        bytes calldata message
+    ) external virtual override onlySystem {
+        address targetTokenAddress;
+        bytes memory recipientAddress;
+
+        if (context.chainID == BITCOIN) {
+            targetTokenAddress = BytesHelperLib.bytesToAddress(message, 0);
+            recipientAddress = abi.encodePacked(
+                BytesHelperLib.bytesToAddress(message, 20)
+            );
+        } else {
+            (address targetToken, bytes memory recipient) = abi.decode(
+                message,
+                (address, bytes)
+            );
+            targetTokenAddress = targetToken;
+            recipientAddress = recipient;
+        }
+
+        uint256 outputAmount = SwapHelperLib._doSwap(
+            systemContract.wZetaContractAddress(),
+            systemContract.uniswapv2FactoryAddress(),
+            systemContract.uniswapv2Router02Address(),
+            zrc20,
+            amount,
+            targetTokenAddress,
+            0
+        );
+
+        (address gasZRC20, uint256 gasFee) = IZRC20(targetTokenAddress)
+            .withdrawGasFee();
+
+        if (gasZRC20 != targetTokenAddress) revert WrongGasContract();
+        if (gasFee >= outputAmount) revert NotEnoughToPayGasFee();
+
+        IZRC20(targetTokenAddress).approve(targetTokenAddress, gasFee);
+        IZRC20(targetTokenAddress).withdraw(
+            recipientAddress,
+            outputAmount - gasFee
+        );
+    }
+}
---
+++ zetachain_contracts/template-swap/tasks/interact.ts
@@ -0,0 +1,50 @@
+import { parseEther } from "@ethersproject/units";
+import { getAddress } from "@zetachain/protocol-contracts";
+import { prepareData } from "@zetachain/toolkit/helpers";
+import bech32 from "bech32";
+import { utils } from "ethers";
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
+  const [signer] = await hre.ethers.getSigners();
+  
+  let recipient;
+  try {
+    if (bech32.decode(args.recipient)) {
+      recipient = utils.solidityPack(
+        ["bytes"],
+        [utils.toUtf8Bytes(args.recipient)]
+      );
+    }
+  } catch (e) {
+    recipient = args.recipient;
+  }
+
+  const data = prepareData(
+    args.contract,
+    ["address", "bytes"],
+    [args.targetToken, recipient]
+  );
+  const to = getAddress("tss", hre.network.name);
+  const value = parseEther(args.amount);
+
+  const tx = await signer.sendTransaction({ data, to, value });
+
+  if (args.json) {
+    console.log(JSON.stringify(tx, null, 2));
+  } else {
+    console.log(`🔑 Using account: ${signer.address}\n`);
+
+    console.log(`🚀 Successfully broadcasted a token transfer transaction on ${hre.network.name} network.
+📝 Transaction hash: ${tx.hash}
+`);
+  }
+};
+
+task("interact", "Interact with the contract", main)
+  .addParam("contract", "The address of the withdraw contract on ZetaChain")
+  .addParam("amount", "Amount of tokens to send")
+  .addFlag("json", "Output in JSON")
+  .addParam("targetToken")
+  .addParam("recipient")
