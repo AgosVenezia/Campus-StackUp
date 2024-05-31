81e7afc25be7cbb3a26fa813e02dd2a6dcbb80be
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2024-05-31 11:57:46 -0300

    Getting Your Hands Dirty With Rust - Quest 1 - Learning The Rust Package Manager Cargo

---
+++ banking-system/target/debug/banking-system.d
@@ -0,0 +1,1 @@
+F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\banking-system\target\debug\banking-system.exe: F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\banking-system\src\main.rs
---
+++ banking-system/target/debug/deps/banking_system.d
@@ -0,0 +1,5 @@
+F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\banking-system\target\debug\deps\banking_system.exe: src\main.rs
+
+F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\banking-system\target\debug\deps\banking_system.d: src\main.rs
+
+src\main.rs:
