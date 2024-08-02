85d89fbf631cd20d148104a874b3101a3eb70090
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2024-08-02 16:52:50 -0300

    Towards Wallet Solutions in Web3 - Quest 3 - Building a dApp with a Wallet Modal

--- bookshelf-dapp/frontend/src/App.js
+++ bookshelf-dapp/frontend/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { createThirdwebClient, getContract } from "thirdweb";
+// We need this for our RPC. We have to tell our ThirdWeb SDK
+// that our smart contracts are deployed to Hardhat testnet for our transactions
+import { hardhat } from "thirdweb/chains";
+// This is needed, the ConnectEmbed has an embeeded Wallet Modal
+import { ConnectEmbed, ThirdwebProvider } from "thirdweb/react";
+// Used to define our wallets. Feel free to experiment after
+// this
+import { createWallet } from "thirdweb/wallets";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// We need this for our ABI so that it can be used
+// for our getContract function to export our smart contracts
+// to our JS code
+import contractData from "./contracts/BookShelf.json";
+
+// This is needed to use ThirdWeb SDK
+const client = createThirdwebClient({
+	// We will use the value from our `.env.local`
+	clientId: process.env.REACT_APP_CLIENT_ID,
+});
+
+// Install this wallets later for experimental purposes
+// We will be using Metamask for the most part
+const wallets = [
+	createWallet("io.metamask"),
+	createWallet("app.phantom"),
+	createWallet("me.rainbow"),
+];
+
+// Our first smart contract on the network
+const contract1 = getContract({
+	client,
+	chain: hardhat,
+	address: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+	abi: contractData.abi,
+});
+
+// Our second smart contract on the network
+const contract2 = getContract({
+	client,
+	chain: hardhat,
+	address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+	abi: contractData.abi,
+});
+
+const AppBase = () => {
+	return <h1>BookShelf</h1>;
+};
+
+const App = () => {
+	// We need to pass the variables here for ConnectEmbed, otherwise, this will error.
+	return (
+		<div className="container">
+			<ThirdwebProvider>
+				<AppBase />
+				<ConnectEmbed
+					chain={hardhat}
+					modalSize={"wide"}
+					client={client}
+					wallets={wallets}
+				/>
+			</ThirdwebProvider>
+		</div>
+	);
+};
 
 export default App;
