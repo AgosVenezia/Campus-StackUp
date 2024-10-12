2bf3581134d646062a3007fbc950d41f4f6ea15a
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2024-10-12 16:02:45 -0300

    Empower Your Developer Journey with Rust - Quest 1 - Tiptoeing Into Rust

--- hello-rust/target/debug/deps/hello_rust-12fd2d9b542e49a7.d
+++
@@ -1,5 +0,0 @@
-F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\hello-rust\target\debug\deps\libhello_rust-12fd2d9b542e49a7.rmeta: src\main.rs
-
-F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\hello-rust\target\debug\deps\hello_rust-12fd2d9b542e49a7.d: src\main.rs
-
-src\main.rs:
--- hello-rust/target/debug/deps/hello_rust-89c8a3cb65cd424f.d
+++
@@ -1,5 +0,0 @@
-F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\hello-rust\target\debug\deps\libhello_rust-89c8a3cb65cd424f.rmeta: src\main.rs
-
-F:\Descargas\Cursos\VSC_PROYECTOS\StackUpCampus\hello-rust\target\debug\deps\hello_rust-89c8a3cb65cd424f.d: src\main.rs
-
-src\main.rs:
