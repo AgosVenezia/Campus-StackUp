a430c03dfc3fb2c9000eea45cdad5601d7813215
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2025-01-15 16:57:17 -0300

    Embark On Your aelf Journey - Quest 3 - Write Your First Smart Contract on aelf

--- first_contract_aelf/src/obj/Debug/net6.0/MyCharacter.AssemblyInfo.cs
+++ first_contract_aelf/src/obj/Debug/net6.0/MyCharacter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCharacter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d50d968e0aba0155a9236738157734c25d828bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce1b99b556476cf7ce5b1cb7d3b207835739547")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCharacter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCharacter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
